29797921074753db41e4839980a1e75ec81be4db
Alimossim <agaev.alimosim@gmail.com>
2022-06-26 15:42:27 +0600

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+  n = Math.abs(n); // Преобразуем отрицательное число
+  n = n.toString(); // Преобразуем в строку
+  n = n.split(''); // Преобразуем в массив
+  n = n.reverse(); // Используем метод реверса массива
+  n = n.join(''); // Обратко в строку
+  return n;      
+};
